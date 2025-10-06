8435eb0d21a7e80f9195ca6ba38c5ae9981772e8
Niinyah <petermarbell@gmail.com>
2025-10-06 13:10:04 +0200

    Commit med test og pom fil ændringer

--- src/test/java/ek/ianb/countries/CountriesApplicationTests.java
+++ src/test/java/ek/ianb/countries/CountriesApplicationTests.java
@@ -2,12 +2,14 @@ package ek.ianb.countries;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-//@SpringBootTest
-//class CountriesApplicationTests {
+@SpringBootTest
+@ActiveProfiles("test")
+class CountriesApplicationTests {
 
-    //@Test
-    //void contextLoads() {
-   // }
+    @Test
+    void contextLoads() {
+    }
 
-//}
+}
